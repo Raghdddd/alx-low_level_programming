4c9553d3af6f86ab5b1f2c69142b12f11c29de77
Raghdddd <raghdahmed033@gmail.com>
2023-09-12 10:22:50 -0700

    999

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,4 @@
-#define main_h
-
+#ifndef MAIN.H
+#define MAIN.H
 int _putchar(char c);
-void print_alphabet (void);
-void print_alphabet_×10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_ bauer (void);
-void times_table(void);
-int add(int, int);
-void print_to 98(int n);
-void print_times_table(int n);
-
 #endif
