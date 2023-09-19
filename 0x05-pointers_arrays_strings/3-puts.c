03eccf598ed78319936be604b56e4945c7868d86
Raghdddd <raghdahmed033@gmail.com>
2023-09-18 17:31:22 -0700

    trnm

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * _puts - prints a string, followed by a new line, to stdout
  *
- * Return: Always 0.
+ * @str: string to print
  */
-int main(void)
-{
-    char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
