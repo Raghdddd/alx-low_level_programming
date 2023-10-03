3e4464bb9bdd74c9049fd8c2697f6a648b9cd474
Raghdddd <raghdahmed033@gmail.com>
2023-10-02 17:22:44 -0700

    'hgt

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - ytr
+ *
+ * @argc: wsd
+ *
+ * @argv: agr
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int f;
+
+	for (f = 0; f < argc; f++)
+		printf("%s\n", argv[f]);
+	return (0);
+}
