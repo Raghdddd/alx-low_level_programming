9988a48c4724a23b408b015892acb9869868c27b
Raghdddd <raghdahmed033@gmail.com>
2023-10-02 17:17:57 -0700

    ewoi

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - do
+ *
+ * @argc: like that
+ *
+ * @argv: when i
+ *
+ * Return: call you senorita
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
