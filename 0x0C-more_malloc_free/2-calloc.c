bb29ed8f1618e8d7f1fd0cbc8a557e0c7a266f81
Raghdddd <raghdahmed033@gmail.com>
2023-10-08 16:26:01 -0700

    ghbvg

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * *_memset - oppaaya
+ * *_memset - gates
  *
- * @s: digaa chinga
- * @b: chowaha saram
- * @n: deerrtt
+ * @s: will
+ * @b: open
+ * @n: tomorrow
  *
- * Return: gttr
+ * Return: right here
  */
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -20,4 +20,29 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 
 	return (s);
+}
+
+/**
+ * *_calloc - wait a minute
+ *
+ * @nmemb: i think
+ * @size: i lost
+ *
+ * Return: my mind
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(size * nmemb);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
