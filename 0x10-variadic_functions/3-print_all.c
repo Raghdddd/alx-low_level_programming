d368394bf4ee91e0f860edc0e50e8ead718f6b50
Raghdddd <raghdahmed033@gmail.com>
2023-10-12 14:09:57 -0700

    gtrwq

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,105 +1,105 @@
-#include "variadic_functions.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
-  * print_all - the
-  *
-  * @format: snowman
-  *
-  * Return: and me
-  */
+ * op_c - jam3a 
+ *
+ * @form: t5mk
+ *
+ * Return: inty wa omk
+ */
+
+void op_c(va_list form)
+{
+	printf("%c", va_arg(form, int));
+}
+/**
+ * op_i - this
+ *
+ * @form: generation
+ *
+ * Return: maj
+ */
+
+void op_i(va_list form)
+{
+	printf("%i", va_arg(form, int));
+}
+/**
+ * op_f - raghd
+ *
+ * @form: in ur
+ *
+ * Return: area
+ */
+
+void op_f(va_list form)
+{
+	printf("%f", va_arg(form, double));
+}
+/**
+ * op_s - mn 7or
+ *
+ * @form: al
+ *
+ * Return: jenan
+ */
+
+void op_s(va_list form)
+{
+	char *str;
+
+	str = va_arg(form, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - he
+ *
+ * @format: was
+ *
+ * Return: hurting inside
+ */
+
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	f_dt form_types[] = {
-		{ "c", print_a_char },
-		{ "i", print_a_integer },
-		{ "f", print_a_float },
-		{ "s", print_a_char_ptr }
-	};
-	unsigned int i = 0;
-	unsigned int j = 0;
+
+	va_list all;
+	unsigned int i, j;
 	char *separator = "";
 
-	va_start(args, format);
+	f ops[] = {
+		{"c", op_c},
+		{"i", op_i},
+		{"f", op_f},
+		{"s", op_s},
+		};
 
-	while (format != NULL && format[i])
+	va_start(all, format);
+	i = 0;
+	while (format && format[i])
 	{
 		j = 0;
 		while (j < 4)
 		{
-			if (format[i] == *form_types[j].identifier)
+			if (ops[j].op[0] == format[i])
 			{
-				form_types[j].f(separator, args);
+				printf("%s", separator);
 				separator = ", ";
+				ops[j].f(all);
+				break;
 			}
 			j++;
 		}
-		i++;
+	i++;
 	}
 
-	va_end(args);
 	printf("\n");
+	va_end(all);
 }
-
-/**
-  * print_a_char - sam7a
-  *
-  * @separator: t3ali
-  * @args: agifi
-  *
-  * Return: yb5rok
-  */
-void print_a_char(char *separator, va_list args)
-{
-	printf("%s%c", separator, va_arg(args, int));
-}
-
-/**
-  * print_a_integer - grab
-  *
-  * @separator: the
-  * @args: red
-  *
-  * Return: bill
-  */
-void print_a_integer(char *separator, va_list args)
-{
-	printf("%s%i", separator, va_arg(args, int));
-}
-
-/**
-  * print_a_float - alright
-  *
-  * @separator: gentman
-  * @args: i want
-  *
-  * Return: to repeat
-  */
-void print_a_float(char *separator, va_list args)
-{
-	printf("%s%f", separator, va_arg(args, double));
-}
-
-/**
-  * print_a_char_ptr - asli
-  *
-  * @separator: look
-  * @args: at
-  *
-  * Return: me
-  */
-void print_a_char_ptr(char *separator, va_list args)
-{
-	char *arg = va_arg(args, char *);
-
-	if (arg == NULL)
-	{
-		printf("%s%s", separator, "(nil)");
-		return;
-	}
-
-	printf("%s%s", separator, arg);
-}
-
