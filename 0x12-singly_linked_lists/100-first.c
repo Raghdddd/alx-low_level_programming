d4fc1e05d939e2b278140c7d0543f04413845360
raghdddd <raghdahmed033@gmail.com>
2023-10-19 07:19:24 -0700

    jhtrewqlk

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "lists.h"
+
+/**
+ * before_main - no rich
+ *
+ * Return: no return.
+ *
+ */
+
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
