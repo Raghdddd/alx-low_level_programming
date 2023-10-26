fa76271c9ffee0280da5d1137c1740bb2a5e70f5
raghdddd <raghdahmed033@gmail.com>
2023-10-26 12:20:53 -0700

    gfrewq

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - i
+ *
+ * @b: know
+ *
+ * Return: you know
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+	int base = 1, k = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[k + 1])
+	{
+		if (b[k] != '0' && b[k] != '1')
+			return (0);
+		k++;
+	}
+
+	while (k >= 0)
+	{
+		res += ((b[k] - '0') * base);
+		base *= 2;
+		k--;
+	}
+
+
+	return (res);
+}
