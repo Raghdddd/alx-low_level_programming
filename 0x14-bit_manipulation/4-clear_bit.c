b07a398b07927a27967b3837d3555b5adc3d4fbd
raghdddd <raghdahmed033@gmail.com>
2023-10-26 13:40:36 -0700

    hgtrbvf

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * clear_bit - we
+ *
+ * @n: just
+ *
+ * @index: friends
+ *
+ * Return: what
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
+	return (1);
+}
